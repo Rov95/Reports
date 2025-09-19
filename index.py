f9a9c8e5bc4f33e73718362f507110d9c0b3ee6c
Rov95 <andres.rov.m.s@gmail.com>
2025-09-18 20:05:56 -0500

    Report running

--- index.py
+++ index.py
@@ -1,1 +1,19 @@
-print('hola')
+from ydata_profiling import ProfileReport
+import pandas as pd
+ 
+data = pd.read_excel('files/consolidados_IPM.xlsx')
+
+data["Cluster"].describe()
+# print('hola')
+
+# report = ProfileReport(data)
+# report.to_file("report.html")
+
+
+if __name__ == "__main__":
+
+    # print(data.info())
+    data["Cluster"].describe()
+    profile = ProfileReport(data, title="v1")
+    profile.te_notebook.iframe()
+    profile.to_file("v1.html")
